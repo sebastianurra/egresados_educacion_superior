07e76a39491b2781df41a620f67dc633cd924d0b
sebastianurra <sebastian.urra.pizarro@gmail.com>
2023-11-22 18:21:12 -0300

    pequeñas modificaciones

--- get_excel_resumen_es.py
+++ get_excel_resumen_es.py
@@ -1,4 +1,5 @@
 import pandas as pd 
+import openpyxl
 
 #Lee el archivo y lo transforma en un df
 df_educ_sup=pd.read_csv('20230714_Titulados_Ed_Superior_2022_WEB.csv',sep=';',header=0)
@@ -7,7 +8,8 @@ df_educ_sup=pd.read_csv('20230714_Titulados_Ed_Superior_2022_WEB.csv',sep=';',he
 df_educ_sup=df_educ_sup[df_educ_sup['nivel_carrera_2']=='Carreras Profesionales'] 
 
 #Realiza un group by por la columna nomb_titulo_obtenido y transforma a excel 
-df_count_carreras=df_educ_sup.groupby('nomb_titulo_obtenido').reset_index(name='Conteo')
+df_count_carreras=df_educ_sup.groupby('nomb_titulo_obtenido').size().reset_index(name='Conteo')
+
 df_count_carreras.to_excel('resumen_educacion_superior_2022.xlsx',index=False,sheet_name='res_carr')
 
 #Realiza un group by por nomb_inst y nomb_titulo_obtenido y luego lo escribe en una hoja del excel creado anteriormente
